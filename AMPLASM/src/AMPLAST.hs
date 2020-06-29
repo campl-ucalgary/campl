d8952a872ae21fd84b94ba2b413cb77d333dcafa
jared <jpon1234@gmail.com>
2020-06-29 01:06:42 -0600

    lens should be together

--- AMPLASM/src/AMPLAST.hs
+++ AMPLASM/src/AMPLAST.hs
@@ -127,9 +127,9 @@ translateCOMToACom n = case n of
             -- tail . init is used to remove the single quote '
             let v' = case v of
                         "\\\\" -> '\\' 
-                        "\'" -> '\\' 
-                        "\n" -> '\\' 
-                        "\t" -> '\\' 
+                        "\'" -> '\'' 
+                        "\n" -> '\n' 
+                        "\t" -> '\t' 
                         [c] -> c
             in AChar (swap a) v'
     AC_STRING (ConstString a) str ->
@@ -155,7 +155,7 @@ translateCOMToACom n = case n of
     AC_EQ (EqI a) ->
         AEqInt (swap a)
     AC_LEQC (Leqc a) ->
-        AEqChar (swap a)
+        ALeqChar (swap a)
     AC_EQC (Eqc a) ->
         AEqChar (swap a)
 
@@ -251,3 +251,145 @@ translateLABELCOMStoALabelComs n = case n of
         ALabelComsArgs 
             ((uIdentToIdent a, uIdentToIdent b), map pIdentToIdent cs) 
             (map translateCOMToACom ds)
+
+translateAComToCOM :: ACom -> COM 
+translateAComToCOM n = case n of
+    AAssign a b -> AC_ASSIGN (coerce $ swap a) (translateAComToCOM b)
+    AStore a b -> AC_STOREf (Store $ swap a) (coerce $ swap b)
+    ALoad a b -> AC_LOADf (Load $ swap a) (coerce $ swap b)
+    ARet a -> AC_RET (Ret $ swap a) 
+    ACall a b cs -> 
+        AC_CALLf (Call $ swap a) (coerce $ swap b) (map (coerce . swap) cs)
+    -- uh oh! not a true isomorphism -- but it should be okay...
+    AInt a v -> 
+        AC_INT 
+            (ConstInt $ swap a) 
+            (if v < 0 
+                then Negative (PInteger ((-1,-1), show (negate v)))
+                else Positive (PInteger ((-1,-1), show v))
+            )
+    AChar a v -> 
+        let v' = case v of
+                    '\\' -> "\\\\" 
+                    '\'' -> "\'" 
+                    '\n' -> "\n" 
+                    '\t' -> "\t" 
+                    c -> [c]
+        in AC_CHAR (ConstChar $ swap a) (Character ((-1,-1), v'))
+        
+    AString a str -> AC_STRING (ConstString $ swap a) str 
+
+    AToStr a -> AC_TOSTR (ToStr $ swap a) 
+    AToInt a -> AC_TOINT (ToInt $ swap a) 
+        
+    AAnd a -> AC_AND (And $ swap a) 
+    AOr a -> AC_OR (Or $ swap a) 
+    AAppend a -> AC_APPEND (Append $ swap a) 
+    ATrue a -> AC_TRUE (BTrue $ swap a) 
+    AFalse a -> AC_FALSE (BFalse $ swap a)
+
+    {-
+    AC_UNSTRING (Unstring a) ->
+        error "no AC_UNSTRING instance"
+    -}
+    ALeqInt a -> AC_LEQ (LeqI $ swap a) 
+    AEqInt a -> AC_EQ (EqI $ swap a) 
+    ALeqChar a -> AC_LEQC (Leqc $ swap a) 
+    AEqChar a -> AC_EQC (Eqc $ swap a) 
+
+    {-
+    AC_LEQS (Leqs a) ->
+        error "no AC_LEQS instance"
+    AC_EQS (Eqs a) -> 
+        error "no AC_EQS instance"
+    AC_CONCAT (ConcatS a) b ->
+        error "no AC_CONCAT instance"
+    -}
+        
+    AAddInt a -> AC_ADD (Add $ swap a) 
+        
+    ASubInt a -> AC_SUB (Subtract $ swap a) 
+    AMulInt a -> AC_MUL (Mul $ swap a) 
+    AModInt a -> AC_DIVQ (Quot $ swap a) 
+    ADivInt a -> AC_DIVR (Rem $ swap a) 
+
+    {-
+    AC_CONS (Cons a) b c ->
+        error "no AC_CONS instance"
+    -}
+
+    AConstructor (a,b) -> AC_STRUCT (coerce $ swap a) (coerce $ swap b)
+    AConstructorArgs ((a, b), args) ->
+        AC_STRUCTAS (coerce $ swap a) (coerce $ swap b) (map (coerce. swap) args) 
+
+    ACase a (b,  cs) ->
+        AC_CASEf (Case $ swap a) (coerce $ swap b) (map translateALabelComsToLABELCOMS cs) 
+    AIf a (b , (cs , ds)) -> 
+        AC_IF (If $ swap a) (coerce $ swap b) (Prog $ map translateAComToCOM cs) (Prog $ map translateAComToCOM ds) 
+    ARecord a bs ->
+        AC_RECORDf (Rec $ swap a) (map translateALabelComsToLABELCOMS bs) 
+
+    ADest (a,b) c -> 
+        AC_DEST (coerce $ swap a) (coerce $ swap b) (coerce $ swap c)
+    ADestArgs ((a, b), cs) d -> 
+        AC_DESTAS (coerce $ swap a) (coerce $ swap b) (map (coerce . swap) cs) (coerce $ swap d)
+
+    AGet a b c -> 
+        AC_GETf (Get $ swap a) (coerce $ swap b) (coerce $ swap c) 
+    AHPut a (b,c) d ->
+        AC_HPUTf (Hput $ swap a) (coerce $ swap b) (coerce $ swap c) (coerce $ swap d) 
+
+    AHCase a (b, cs) -> 
+        AC_HCASEf (Hcase $ swap a) (coerce $ swap b) (map translateALabelComsToLABELCOMS cs)
+    APut a b c -> 
+        AC_PUTf (Put $ swap a) (coerce $ swap b) (coerce $ swap c) 
+
+    ASplit a b c d ->
+        AC_SPLITf (Split $ swap a) (coerce $ swap b) (coerce $ swap c) (coerce $ swap d) 
+
+
+    AFork a b ((a1, a1s), as) ((b1, b1s), bs) ->
+        AC_FORKf
+            (Fork $ swap a) (coerce $ swap b) 
+            (coerce $ swap a1) 
+            (map (coerce . swap) a1s) (Prog $ map translateAComToCOM as) 
+            (coerce $ swap b1) (map (coerce . swap) b1s) (Prog $ map translateAComToCOM bs)  
+
+    APlug a as (a1s, a2s) (b1s, b2s) -> 
+        AC_PLUGf (Plug $ swap a) (map (coerce . swap) as) 
+            (map (coerce . swap) a1s) (Prog $ map translateAComToCOM a2s) 
+            (map (coerce . swap)  b1s) (Prog $ map translateAComToCOM b2s) 
+
+    ARun a b (cs, (ds, es)) ->
+        AC_RUNf (Run $ swap a) (coerce $ swap b) (map (coerce . swap) cs) (map (coerce . swap) ds) (map (coerce . swap) es) 
+
+    AId b (a,c) -> 
+        AC_IDF (coerce $ swap a) (Ch_Id $ swap b) (coerce $ swap c) 
+
+    ARace a bs -> 
+        AC_RACE (Race $ swap a) (map f bs) 
+      where
+        f :: (Ident, [ACom]) -> RACES
+        f (a, coms) = (Races (coerce $ swap a) (Prog $ map translateAComToCOM coms)) 
+
+    AClose a b -> 
+        AC_CLOSEf (Close $ swap a) (coerce $ swap b) 
+    AHalt a b -> 
+        AC_HALTf (Halt $ swap a) (coerce $ swap b) 
+
+    {-
+    AC_PROD as -> AProd (map pIdentToIdent as)
+    AC_PRODELEM a b -> AProdElem (pIdentToIdent b) (pIntegerToWord a)
+
+    AC_EMSG str ->
+        AErrMsg str
+        
+-}
+
+translateALabelComsToLABELCOMS :: ALabelComs -> LABELCOMS 
+translateALabelComsToLABELCOMS n = case n of
+    ALabelComs (a,b) cs -> 
+        Labelcoms1 (coerce $ swap a) (coerce $ swap b) (Prog $ map translateAComToCOM cs) 
+        
+    ALabelComsArgs ((a,b),  cs)  ds ->
+        Labelcoms2 (coerce $ swap a) (coerce $ swap b) (map (coerce . swap) cs) (Prog $ map translateAComToCOM ds) 
