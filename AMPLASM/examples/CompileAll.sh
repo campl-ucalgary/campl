a3274dae018235bc2bf1745854429aceb086a880
SgtWiggles <younye1000@gmail.com>
2020-08-21 12:44:52 -0600

    Standardized file extensions

--- AMPLASM/examples/CompileAll.sh
+++ AMPLASM/examples/CompileAll.sh
@@ -1,9 +1,9 @@
 #!/usr/local/bin/bash
 shopt -s nullglob
-for file in *.ampl; do
+for file in *.ampla; do
 	filename="${file%.*}"
 	printf "[asm] Compiling $file... "
-	if stack exec amplasm -- "$file" "$filename.amplasm"; then
+	if stack exec amplasm -- "$file" "$filename.amplm"; then
 		printf "Success!\n"
 	fi
 done
