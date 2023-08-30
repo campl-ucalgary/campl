09a4b8739ba0779aa6dbfc3ec02b90307572c744
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2023-08-29 21:39:02 -0600

    fix: fix installation script

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if ! command -v stack &> /dev/null
-    then
-        echo "Installation failed. Please make sure that you have 'stack' installed"
-    else
-        echo "CaMPL was installed Successfully!";
-fi
